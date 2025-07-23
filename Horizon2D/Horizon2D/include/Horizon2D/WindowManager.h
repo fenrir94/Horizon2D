3476a72f170f70557c27e53bfaf1e32441f0a738
Fenr1r <4p0the0sis94@gmail.com>
2025-07-23 15:38:01 +0900

    Apply Singleton to WindowManager

--- Horizon2D/Horizon2D/include/Horizon2D/WindowManager.h
+++ Horizon2D/Horizon2D/include/Horizon2D/WindowManager.h
@@ -1,16 +1,28 @@
 #pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 class WindowManager
 {
 public:
+	static WindowManager& getInstance();
+
+	void Initialize(int widht, int height, const char* title);
+	void ClearWindow();
+	void CreateWindow();
+	void Destroy();
+
+private:
 	WindowManager();
 	~WindowManager();
 
-	void Initialize();
-	void ClearWindow();
+	WindowManager(const WindowManager&) = delete;
+	WindowManager& operator=(const WindowManager&) = delete;
 
 private:
 	unsigned int m_width;
 	unsigned int m_heigth;
+
+	GLFWwindow* m_Window = nullptr;
 };
 
--- Horizon2D/Horizon2D/src/WindowManager.cpp
+++ Horizon2D/Horizon2D/src/WindowManager.cpp
@@ -1,19 +1,45 @@
 #include "WindowManager.h"
+#include "Constants.h"
+
+
 
 WindowManager::WindowManager()
 {
-
+	m_width = Horizon2D::WINDOW_WIDTH;
+	m_heigth = Horizon2D::WINDOW_HIGHT;
 }
 
 WindowManager::~WindowManager()
 {
 }
 
-void WindowManager::Initialize()
+WindowManager& WindowManager::getInstance()
 {
+	static WindowManager instance;
+	return instance;
+}
+
+void WindowManager::Initialize(int width, int height, const char* title)
+{
+	glfwInit();
+	m_Window = glfwCreateWindow(width, height, title, 0, 0);
 }
 
 void WindowManager::ClearWindow()
 {
+	glClearColor(1.f, 1.f, 1.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(m_Window);
+}
 
+void WindowManager::CreateWindow()
+{
+	glfwMakeContextCurrent(m_Window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+}
+
+void WindowManager::Destroy()
+{
+	glfwDestroyWindow(m_Window);
+	glfwTerminate();
 }
