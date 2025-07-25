6e37a52f3530db56a5f43cd4ebfafa7f3658d7aa
Fenr1r <4p0the0sis94@gmail.com>
2025-07-25 18:06:29 +0900

    Update MainMenu.cpp
    Implements Update

--- Horizon2D/MockUp/src/MainMenu.cpp
+++ Horizon2D/MockUp/src/MainMenu.cpp
@@ -1,1 +1,29 @@
 #include "MainMenu.h"
+#include <Horizon2D/Horizon2D.h>
+#include <iostream>
+
+MainMenu::MainMenu()
+{
+
+}
+
+MainMenu::~MainMenu()
+{
+
+}
+
+void MainMenu::Initialize()
+{
+}
+
+void MainMenu::Update(float dt)
+{
+	if (Horizon2D::IsKeyPressed(GLFW_KEY_SPACE))
+	{
+		std::cout << "Main" << std::endl;
+	}
+}
+
+void MainMenu::Exit()
+{
+}
