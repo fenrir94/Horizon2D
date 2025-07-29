3c4b52b94694e38d6255e42277bc9b5698ff7a41
Fenr1r <4p0the0sis94@gmail.com>
2025-07-29 19:13:18 +0900

    Implement functions of Mesh

--- Horizon2D/Horizon2D/include/Horizon2D/Mesh.h
+++ Horizon2D/Horizon2D/include/Horizon2D/Mesh.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <glad/gl.h>
+#include <vector>
+
 class Mesh
 {
+public:
+	Mesh(const std::vector<float>& vertices);
+	void Draw() const;
+
+private:
+	GLuint m_vao;
+	GLuint m_vbo;
 };
 
--- Horizon2D/Horizon2D/src/Mesh.cpp
+++ Horizon2D/Horizon2D/src/Mesh.cpp
@@ -1,1 +1,20 @@
 #include "Mesh.h"
+
+Mesh::Mesh(const std::vector<float>& vertices)
+{
+	glGenVertexArrays(1, &m_vao);
+	glGenBuffers(1, &m_vbo);
+
+	glBindVertexArray(m_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+void Mesh::Draw() const
+{
+	glBindVertexArray(m_vao);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
