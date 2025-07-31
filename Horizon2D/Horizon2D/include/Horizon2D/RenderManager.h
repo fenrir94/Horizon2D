36d760fab5eec9c889d7566211b94c18171cddac
Fenr1r <4p0the0sis94@gmail.com>
2025-07-31 10:54:10 +0900

    Implement RenderManager

--- Horizon2D/Horizon2D/include/Horizon2D/RenderManager.h
+++ Horizon2D/Horizon2D/include/Horizon2D/RenderManager.h
@@ -1,12 +1,20 @@
 #pragma once
-
+#include <memory>
+#include "Mesh.h"
+#include "Shader.h"
+#include "MeshManager.h"
+#include "ShaderManager.h"
 
 class RenderManager
 {
 public:
+
+    void Initialize();
+
 	void DrawRectangle(float x, float y, float width, float height, float r, float g, float b, float alpha);
 
 private:
-
+    MeshManager m_MeshManager;
+    ShaderManager m_ShaderManager;
 };
 
--- Horizon2D/Horizon2D/src/RenderManager.cpp
+++ Horizon2D/Horizon2D/src/RenderManager.cpp
@@ -1,1 +1,31 @@
 #include "RenderManager.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+void RenderManager::Initialize()
+{
+	m_MeshManager.Initialize();
+	m_ShaderManager.Initialize();
+}
+
+void RenderManager::DrawRectangle(float x, float y, float width, float height, float r, float g, float b, float alpha)
+{
+	auto mesh = m_MeshManager.LoadRectangle("Rectangle");
+	auto shader = m_ShaderManager.LoadShader("Basic", "shader.vert", "shader.frag");
+
+	shader->Use();
+	
+	glm::mat4 projection = glm::ortho(0.0f, 1000.f, 0.f, 1000.0f, -1.f, 1.f);
+
+	glm::mat4 view = glm::mat4(1.0f);
+	
+	glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(x, y, 0.f));
+	model = glm::scale(model, glm::vec3(width, height, 1.0f));
+	shader->SetUniform("u_Model", model);
+	shader->SetUniform("u_View", view);
+	shader->SetUniform("u_Projection", projection);
+	shader->SetUniform("u_Color", glm::vec4(r, g, b, alpha));
+
+	mesh->Draw();
+}
